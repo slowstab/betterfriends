420989daa468faa87c83df357a0111366347731a
e-boi <hiboysismyoufake6789@gmail.com>
2021-09-24 19:23:31 -0400

    some bug fixes

--- modules/DisplayStar.js
+++ modules/DisplayStar.js
@@ -1,48 +1,47 @@
 const { React, getModule, getModuleByDisplayName } = require('powercord/webpack');
 const { inject } = require('powercord/injector');
 const { Star } = require('./../components');
+const { findInReactTree } = require('powercord/util');
 
 /*
  * [ Display Star ]
  * Handles the displaying of the little star emoji next to favorited friends in both chat and in member lists.
  */
 module.exports = async function () {
-  if (!this.settings.get('displaystar', true)) {
-    return;
-  }
-  const classes = await getModule([ 'profileBadgeStaff' ]);
-  const isFavoriteFriend = (id) => this.FAV_FRIENDS.includes(id);
+	if (!this.settings.get('displaystar', true)) {
+		return;
+	}
+	const classes = await getModule(['topSection']);
+	const isFavoriteFriend = id => this.FAV_FRIENDS.includes(id);
 
-  /**
-   * Thanks to Bowser65 for some of the code provided below
-   */
-  const _injectMembers = async () => {
-    const MemberListItem = await getModuleByDisplayName('MemberListItem')
-    inject('bf-star-members', MemberListItem.prototype, 'renderDecorators', function (args, res) {
-      if (!isFavoriteFriend(this.props.user.id)) return res;
-      res.props.children.unshift(
-        React.createElement('div', { className: `bf-badge ${classes.topSectionNormal}` },
-          React.createElement(Star, { className: 'bf-star-member' })
-        )
-      )
-      return res
-    })
-  }
+	/**
+	 * Thanks to Bowser65 for some of the code provided below
+	 */
+	const _injectMembers = async () => {
+		const MemberListItem = await getModuleByDisplayName('MemberListItem');
+		inject('bf-star-members', MemberListItem.prototype, 'renderDecorators', function (args, res) {
+			if (!isFavoriteFriend(this.props.user.id)) return res;
+			res.props.children.unshift(
+				React.createElement('div', { className: `bf-badge ${classes.topSection}` }, React.createElement(Star, { className: 'bf-star-member' }))
+			);
+			return res;
+		});
+	};
 
-  const _injectMessages = async () => {
-    const MessageTimestamp = await getModule(['MessageTimestamp']);
-    inject('bf-star-messages', MessageTimestamp, 'default', (args, res) => {
-      if (!isFavoriteFriend(args[0].message.author.id)) return res;
-      const header = res.props.children[1];
-      // eslint-disable-next-line prefer-destructuring
-      header.props.children[3] = header.props.children[2];
-      header.props.children[2] = React.createElement('div', { className: `bf-badge ${classes.topSectionNormal}` },
-        React.createElement(Star)
-      )
-      return res
-    });
-  }
+	const _injectMessages = async () => {
+		const MessageTimestamp = await getModule(
+			m =>
+				typeof (m?.__powercordOriginal_default || m.default) === 'function' &&
+				(m?.__powercordOriginal_default || m.default).toString().includes('showTimestampOnHover')
+		);
+		inject('bf-star-messages', MessageTimestamp, 'default', (args, res) => {
+			if (!isFavoriteFriend(args[0].message.author.id)) return res;
+			const header = findInReactTree(res, e => Array.isArray(e?.props?.children) && e.props.children.find(c => c?.props?.message));
+			header.props.children.push(React.createElement('div', { className: `bf-badge ${classes.topSection}` }, React.createElement(Star)));
+			return res;
+		});
+	};
 
-  _injectMembers();
-  _injectMessages();
+	_injectMembers();
+	_injectMessages();
 };
--- modules/FavoriteFriendChannel.js
+++ modules/FavoriteFriendChannel.js
@@ -1,16 +1,16 @@
-const { inject } = require("powercord/injector");
-const { open: openModal } = require("powercord/modal");
+const { inject } = require('powercord/injector');
+const { open: openModal } = require('powercord/modal');
 const {
-  Icons: { Keyboard },
-  Tooltip,
-} = require("powercord/components");
+	Icons: { Keyboard },
+	Tooltip,
+} = require('powercord/components');
 const {
-  React,
-  Flux,
-  getModuleByDisplayName,
-  getModule,
-  constants: { Routes },
-} = require("powercord/webpack");
+	React,
+	Flux,
+	getModuleByDisplayName,
+	getModule,
+	constants: { Routes },
+} = require('powercord/webpack');
 
 const FavoriteFriends = require("../components/FavoriteFriends");
 const InformationModal = require("../components/InformationModal");
@@ -20,165 +20,129 @@ const InformationModal = require("../components/InformationModal");
  * Creates and populates the "Favorited Friends" section on the private channel/DMs screen
  */
 module.exports = async function () {
-  const _this = this;
-  const PrivateChannel = await getModuleByDisplayName("PrivateChannel");
-  const ConnectedPrivateChannelsList = await getModule(
-    (m) => m.default && m.default.displayName === "ConnectedPrivateChannelsList"
-  );
-  const dms = await getModule(["openPrivateChannel"]);
-  const transition = await getModule(["transitionTo"]);
-  const userStore = await getModule(["getUser", "getCurrentUser"]);
-  const channelStore = await getModule(["getChannel", "getDMFromUserId"]);
-  const activityStore = await getModule(["getPrimaryActivity"]);
-  const statusStore = await getModule(["getStatus"]);
-  const nickStore = await getModule(["getNickname"]);
-  const classes = {
-    ...(await getModule(["channel", "closeButton"])),
-    ...(await getModule(["avatar", "muted", "selected"])),
-    ...(await getModule(["privateChannelsHeaderContainer"])),
-  };
+	const _this = this;
+	const PrivateChannel = await getModuleByDisplayName('PrivateChannel');
+	const ConnectedPrivateChannelsList = await getModule(m => m.default && m.default.displayName === 'ConnectedPrivateChannelsList');
+	const dms = await getModule(['openPrivateChannel']);
+	const transition = await getModule(['transitionTo']);
+	const userStore = await getModule(['getUser', 'getCurrentUser']);
+	const channelStore = await getModule(['getChannel', 'getDMFromUserId']);
+	const activityStore = await getModule(['getPrimaryActivity']);
+	const statusStore = await getModule(['getStatus']);
+	const classes = {
+		...(await getModule(['channel', 'closeButton'])),
+		...(await getModule(['avatar', 'muted', 'selected'])),
+		...(await getModule(['privateChannelsHeaderContainer'])),
+	};
 
-  // Patch PrivateChannel
-  inject(
-    "bf-direct-messages-channel",
-    PrivateChannel.prototype,
-    "render",
-    function (args, res) {
-      if (this.props.isBetterFriends) {
-        res.props.children = this.props.infoModal
-          ? React.createElement(
-              Tooltip,
-              {
-                text: "User Information",
-                position: "top",
-              },
-              React.createElement(Keyboard, {
-                className: "bf-information",
-                onClick: (e) => {
-                  e.stopPropagation();
-                  e.preventDefault();
-                  const info =
-                    _this.FRIEND_DATA.lastMessageID[this.props.user.id];
-                  openModal(() =>
-                    React.createElement(InformationModal, {
-                      user: {
-                        ...this.props.user,
-                        isSystemUser: () => false,
-                        isSystemDM: () => false,
-                      },
-                      channel: !info ? "nothing" : info.channel,
-                      message: !info ? "nothing" : info.id,
-                    })
-                  );
-                },
-              })
-            )
-          : React.createElement("p");
+	// Patch PrivateChannel
+	inject('bf-direct-messages-channel', PrivateChannel.prototype, 'render', function (args, res) {
+		// console.log(_this.FAV_FRIENDS.includes(this.props.user?.id))
+		if (_this.FAV_FRIENDS.includes(this.props.user?.id)) {
+			if (!res.props.className.includes('bf-favoritefriend')) res.props.className += ' bf-favoritefriend';
+			if (powercord.api.settings.store.getSetting('betterfriends', 'infomodal'))
+				res.props.children = [
+					React.createElement(
+						Tooltip,
+						{
+							text: 'User Information',
+							position: 'top',
+						},
+						React.createElement(Keyboard, {
+							className: 'bf-information',
+							onClick: e => {
+								e.stopPropagation();
+								e.preventDefault();
+								const info = _this.FRIEND_DATA.lastMessageID[this.props.user.id];
+								openModal(() =>
+									React.createElement(InformationModal, {
+										user: { ...this.props.user, isSystemUser: () => false, isSystemDM: () => false },
+										channel: !info ? 'nothing' : info.channel,
+										message: !info ? 'nothing' : info.id,
+									})
+								);
+							},
+						})
+					),
+					res.props.children,
+				];
 
-        if (this.props.channel.id === "0" && res.props.children) {
-          res.props.onMouseDown = () => void 0;
-          res.props.children = React.createElement(
-            "a",
-            null,
-            res.props.children.props.children
-          );
-          res.props.onClick = async () => {
-            const channelId = await dms.openPrivateChannel(
-              userStore.getCurrentUser().id,
-              this.props.user.id
-            );
-            // eslint-disable-next-line new-cap
-            transition.transitionTo(Routes.CHANNEL("@me", channelId));
-            if (_this.favFriendsInstance)
-              _this.favFriendsInstance.forceUpdate();
-          };
-        }
-      }
-      return res;
-    }
-  );
+			if (this.props.channel.id === '0' && res.props.children) {
+				res.props.onMouseDown = () => void 0;
+				res.props.children = React.createElement('a', null, res.props.children.props.children);
+				res.props.onClick = async () => {
+					const channelId = await dms.openPrivateChannel(userStore.getCurrentUser().id, this.props.user.id);
+					// eslint-disable-next-line new-cap
+					transition.transitionTo(Routes.CHANNEL('@me', channelId));
+					if (_this.favFriendsInstance) _this.favFriendsInstance.forceUpdate();
+				};
+			}
+		}
+		return res;
+	});
 
-  // Build connected component
-  const ConnectedPrivateChannel = Flux.connectStores(
-    [
-      userStore,
-      channelStore,
-      activityStore,
-      statusStore,
-      powercord.api.settings.store,
-    ],
-    ({ userId, currentSelectedChannel }) => {
-      const channelId = channelStore.getDMFromUserId(userId);
-      const selected = currentSelectedChannel === channelId;
-      const user = userStore.getUser(userId) || {
-        id: "0",
-        username: "???",
-        isSystemUser: () => false,
-        getAvatarURL: () => null,
-        isSystemDM: () => false,
-      };
+	// Build connected component
+	const ConnectedPrivateChannel = Flux.connectStores(
+		[userStore, channelStore, activityStore, statusStore, powercord.api.settings.store],
+		({ userId, currentSelectedChannel }) => {
+			const channelId = channelStore.getDMFromUserId(userId);
+			const selected = currentSelectedChannel === channelId;
+			const user = userStore.getUser(userId) || {
+				id: '0',
+				username: '???',
+				isSystemUser: () => false,
+				getAvatarURL: () => null,
+				isSystemDM: () => false,
+			};
 
-      const channel = channelId
-        ? channelStore.getChannel(channelId)
-        : {
-            id: "0",
-            type: 1,
-            isMultiUserDM: () => false,
-            isSystemUser: () => false,
-            isSystemDM: () => false,
-            recipients: [user.id],
-            toString: () => user.username,
-          };
+			const channel = channelId
+				? channelStore.getChannel(channelId)
+				: {
+						id: '0',
+						type: 1,
+						isMultiUserDM: () => false,
+						isSystemUser: () => false,
+						isSystemDM: () => false,
+						recipients: [user.id],
+						toString: () => user.username,
+				  };
 
-      return {
-        user,
-        channel,
-        selected,
-        channelName: nickStore.getNickname(userId) ?? user.username,
-        isMobile: statusStore.isMobileOnline(userId),
-        status: statusStore.getStatus(userId),
-        activities: activityStore.getActivities(userId),
-        infoModal: powercord.api.settings.store.getSetting(
-          "betterfriends",
-          "infomodal"
-        ),
-        isBetterFriends: true,
-      };
-    }
-  )(PrivateChannel);
+			return {
+				user,
+				channel,
+				selected,
+				channelName: user.username,
+				isMobile: statusStore.isMobileOnline(userId),
+				status: statusStore.getStatus(userId),
+				activities: activityStore.getActivities(userId),
+				infoModal: powercord.api.settings.store.getSetting('betterfriends', 'infomodal'),
+				isBetterFriends: true,
+			};
+		}
+	)(PrivateChannel);
 
-  // Patch DM list
-  inject(
-    "bf-direct-messages",
-    ConnectedPrivateChannelsList,
-    "default",
-    (args, res) => {
-      res.props.privateChannelIds = res.props.privateChannelIds.filter((c) => {
-        const channel = channelStore.getChannel(c);
-        return (
-          channel.type !== 1 ||
-          !this.FAV_FRIENDS.includes(channel.recipients[0])
-        );
-      });
+	// Patch DM list
+	inject('bf-direct-messages', ConnectedPrivateChannelsList, 'default', (args, res) => {
+		res.props.privateChannelIds = res.props.privateChannelIds.filter(c => {
+			const channel = channelStore.getChannel(c);
+			return channel.type !== 1 || !this.FAV_FRIENDS.includes(channel.recipients[0]);
+		});
 
-      if (this.favFriendsInstance) this.favFriendsInstance.forceUpdate();
-      res.props.children = [
-        // Previous elements
-        ...res.props.children,
-        // Favorite Friends
-        () =>
-          React.createElement(FavoriteFriends, {
-            classes,
-            ConnectedPrivateChannel,
-            FAV_FRIENDS: this.FAV_FRIENDS,
-            selectedChannelId: res.props.selectedChannelId,
-            _this,
-          }),
-      ];
+		if (this.favFriendsInstance) this.favFriendsInstance.forceUpdate();
+		res.props.children = [
+			// Previous elements
+			...res.props.children,
+			// Favorite Friends
+			() =>
+				React.createElement(FavoriteFriends, {
+					classes,
+					FAV_FRIENDS: this.FAV_FRIENDS,
+					selectedChannelId: res.props.selectedChannelId,
+					_this,
+				}),
+		];
 
-      return res;
-    }
-  );
-  ConnectedPrivateChannelsList.default.displayName =
-    "ConnectedPrivateChannelsList";
+		return res;
+	});
+	ConnectedPrivateChannelsList.default.displayName = 'ConnectedPrivateChannelsList';
 };
